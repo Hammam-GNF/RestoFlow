19ab301ee21d14288fd614df78120ee4d9caf667
Hammam GNF <hammamgonjil@gmail.com>
2026-02-24 01:08:35 +0700

    refactor(auth): implement role middleware and centralize authorization

--- app/Http/Requests/StoreFoodRequest.php
+++ app/Http/Requests/StoreFoodRequest.php
@@ -11,9 +11,7 @@ class StoreFoodRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        $user = $this->user();
-
-        return $user && $user->role === 'pelayan';
+        return true;
     }
 
     /**
